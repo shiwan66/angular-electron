d655624e697d14ed2fe15200ce24df341e0a0bc4
shiwan66 <shiwan66@gmail.com>
2019-03-12 10:33:37 +0800

    页面布局及组件

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AddressDataChinaService } from 'ngx-address/data/china';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  public code: string;
+  public codeName: string;
+  public opt: any;
+  areaName: string; // 范围区域
 
-  constructor() { }
+  public pystr: string;
+  public ipstr: string;
+  
+  title = 'app';
+  elementType = 'url';
+  value = 'Techiediaries';
+
+  constructor(private china: AddressDataChinaService) {
+    this.opt = {
+      jumps: this.china.getJumps(),
+      data: this.china.getData.bind(this.china)
+    };
+  }
 
   ngOnInit() {
   }
 
+  onCustomSelected(result) {
+    if(result && result.paths && result.paths.length) {
+      this.areaName = result.paths[0].name;
+      this.codeName = result.paths.map(item => item.name).join("");
+    }
+  }
 }
